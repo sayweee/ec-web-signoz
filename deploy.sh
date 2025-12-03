f8f6482f1e99957307fa6476a00051ab2aa27f5d
gaozhichao123 <zhichao.gao@sayweee.com>
2025-12-03 11:27:53 +0800

    feat: change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -39,11 +39,7 @@ make build-query-service-static-amd64
 # 关闭运行的前端与收集器容器
 echo "Stopping running containers..."
 # docker stop signoz-otel-collector signoz-frontend signoz-query-service || {
-docker stop signoz-otel-collector signoz-frontend signoz-query-service signoz-logspout signoz-alertmanager signoz-clickhouse signoz-zookeeper-1 || {
-# docker stop $(docker ps -q) || {
-    echo "Failed to stop the containers."
-    exit 1
-}
+docker stop signoz-otel-collector signoz-frontend signoz-query-service signoz-logspout signoz-alertmanager signoz-clickhouse signoz-zookeeper-1 2>/dev/null || true
 
 # 删除所有停止的容器和未使用的镜像
 echo "delete unuse containers & images..."
